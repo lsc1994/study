7364a272aa84b1f2a03df7170e4dfab5a9dc31dd
Lee <seungchol3@gmail.com>
2022-03-07 18:33:58 +0900

    s

--- Quest04/skeleton/desktop.js
+++ Quest04/skeleton/desktop.js
@@ -1,9 +1,21 @@
 class Desktop {
 	/* TODO: Desktop 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+	constructor(move){
+		this.move = move;
+	}
+	move(){
+
+	}
 };
 
 class Icon {
 	/* TODO: Icon 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+	constructor(icon){
+		this.icon = icon;
+	}
+	icon(){
+		return document.write("<div>asdf</div>");
+	}
 };
 
 class Folder {
